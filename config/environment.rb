ccb130d46dc72781d4d8016b92bdf29880ab34ba
Varvara <solarmousse@gmail.com>
2012-12-10 21:15:39 +0400

    heroku v_3

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,10 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Lab3::Application.initialize!
+
+require 'hassle'
+
+Rails::Initializer.run do |config|
+
+     config.middleware.use Hassle
+end
